d9e33f0f235b30e472e585d14376a482a63fb4a1
owlProjects <a.mikheev93@gmail.com>
2025-03-27 22:02:39 +0300

    добавлен CalcSceneBuilder

--- Lab1/Lab1Java/src/Program.java
+++ Lab1/Lab1Java/src/Program.java
@@ -1,4 +1,5 @@
 import graph.AbstractGOFactory;
+import graph.CalcSceneBuilder;
 import graph.ColorGOFactory;
 import graph.RealSceneBuilder;
 import graph.Scene;
@@ -13,10 +14,21 @@ public class Program {
 		gof.createCircle().setColor("blue");
 		gof.createLine().setColor("purple");
 		
-		new RealSceneBuilder();
-
 		Scene.instance.draw();
 
+		var realBuilder = new RealSceneBuilder();
+		realBuilder.reset();
+		realBuilder.addPoints();
+		realBuilder.addLines();
+		realBuilder.addCircles();
+		Scene.instance.draw();
+
+		var calcBuilder = new CalcSceneBuilder();
+		calcBuilder.reset();
+		calcBuilder.addPoints();
+		calcBuilder.addLines();
+		calcBuilder.addCircles();
+		calcBuilder.finish();
 	}
 
 }
--- Lab1/Lab1Java/src/graph/CalcSceneBuilder.java
+++ Lab1/Lab1Java/src/graph/CalcSceneBuilder.java
@@ -1,7 +1,8 @@
 package graph;
 
-public class RealSceneBuilder implements SceneBuilder {
+public class CalcSceneBuilder  implements SceneBuilder {
     private final Scene scene = Scene.instance;
+    private int size;
 
 	@Override
     public void reset() {
@@ -11,17 +12,20 @@ public class RealSceneBuilder implements SceneBuilder {
 	public void addPoints() {
         scene.add(new Point(1,1));
         scene.add(new Point(2,2));
+        size = 2;
     }
     @Override
 	public void addLines() {
         scene.add(new Line(1,1,2,2));
+        size++;
     }
     @Override
 	public void addCircles() {
         scene.add(new Circle(1,1,5));
+        size++;
     }
     @Override
 	public void finish() {
-        scene.draw();
+        System.out.printf("Scene size: %d objects", size);
     }
 }
